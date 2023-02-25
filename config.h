250b95ec4456c1cffcc86bbc3a6e7ff54a7ce050
aidancz <aidanczhai@gmail.com>
2023-02-25 16:57:46 +0800

    xresources font fix

--- config.h
+++ config.h
@@ -16,7 +16,10 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=15", "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true" };
+static char fonts_0[]            = "monospace:size=15";
+static char fonts_1[]            = "Noto Sans CJK SC:pixelsize=18";
+static char fonts_2[]            = "Twemoji:pixelsize=15";
+static char *fonts[]          = { fonts_0, fonts_1, fonts_2 };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -111,6 +114,9 @@ static const char *termcmd[]  = { TERMINAL, NULL };
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
+		{ "fonts_0",		STRING,	&fonts_0 },
+		{ "fonts_1",		STRING,	&fonts_1 },
+		{ "fonts_2",		STRING,	&fonts_2 },
 		{ "normbgcolor",	STRING,	&normbgcolor },
 		{ "normfgcolor",	STRING,	&normfgcolor },
 		{ "selbgcolor",		STRING,	&selbgcolor },
