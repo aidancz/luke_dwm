6141e75b32d96ae2377bf54a875ee6c9d914145a
aidancz <aidanczhai@gmail.com>
2023-06-28 12:40:05 +0800

    font

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static int smartgaps          = 1;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
 static char fonts_0[]            = "monospace:size=15";
-static char fonts_1[]            = "Noto Sans CJK SC:pixelsize=18";
-static char fonts_2[]            = "Twemoji:pixelsize=15";
+static char fonts_1[]            = "monospace:size=15";
+static char fonts_2[]            = "monospace:size=15";
 static char *fonts[]          = { fonts_0, fonts_1, fonts_2 };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
